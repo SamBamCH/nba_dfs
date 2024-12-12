4ff5ea9a3d56fea1979883f90c41e239fe8237df
SamBamCH <SamBamCH@GMail.com>
2024-12-12 16:27:01 -0600

    i should really test code before i push it. This commit undoes the last fix.

--- src/main.py
+++ src/main.py
@@ -36,14 +36,14 @@ def main():
     optimizer = Optimizer(site, players, num_lineups, num_uniques, data_manager.config)
 
     # Generate lineups
-    Lineups = optimizer.run()
+    lineups = optimizer.run()
 
     # Calculate and display player exposure
-    exposure_df = calculate_exposure(Lineups.lineups, players)
+    exposure_df = calculate_exposure(lineups.lineups, players)
     print(exposure_df)
 
     # Export the lineups
-    Lineups.export_to_csv("data/output/optimal_lineups.csv", site=optimizer.site)
+    lineups.export_to_csv("data/output/optimal_lineups.csv", site=optimizer.site)
 
 
 if __name__ == "__main__":
